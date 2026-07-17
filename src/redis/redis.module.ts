e38266d53029e358e23e0570e83fe4d4d4671c1f
Netanelcoh <netanelc96@gmail.com>
2026-07-17 15:17:03 +0300

    create Redis shared module

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,31 @@
+import { Global, Module, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ModuleRef } from '@nestjs/core';
+import Redis from 'ioredis';
+
+export const REDIS_CLIENT = 'REDIS_CLIENT';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: REDIS_CLIENT,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): Redis =>
+        new Redis({
+          host: config.get<string>('REDIS_HOST', 'localhost'),
+          port: config.get<number>('REDIS_PORT', 6379),
+          maxRetriesPerRequest: null,
+        }),
+    },
+  ],
+  exports: [REDIS_CLIENT],
+})
+export class RedisModule implements OnApplicationShutdown {
+  constructor(private readonly moduleRef: ModuleRef) {}
+
+  async onApplicationShutdown(): Promise<void> {
+    const client = this.moduleRef.get<Redis>(REDIS_CLIENT);
+    await client.quit();
+  }
+}
